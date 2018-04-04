837b7e72afa4ef2ed3d88e697f7fa58140187590
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-04 16:02:06 +0530

    Test SnS commit 2

--- TestingSigma/lambda.js
+++ TestingSigma/lambda.js
@@ -1,10 +1,38 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
 	let reciever = event['reciever'];
 	let sender = event['sender'];
 	let message = event['message']
 
-	console.log("Succesfully message ", message," for",reciever, ' from ',sender);
-	callback(null,'Successfully executed');
+	console.log("Succesfully message ", message, " for", reciever, ' from ', sender);
+
+	sns.publish({
+		Message: message,
+		MessageAttributes: {
+			'AWS.SNS.SMS.SMSType': {
+				DataType: 'String',
+				StringValue: 'Promotional'
+			},
+			'AWS.SNS.SMS.SenderID': {
+				DataType: 'String',
+				StringValue: sender
+			},
+		},
+		PhoneNumber: reciever
+	}).promise()
+		.then(data => {
+			// your code goes here
+			console.log("Sending success to ",reciever);
+			callback(null,data);
+		})
+		.catch(err => {
+			// error handling goes here
+			console.log("sending failed",err);
+			callback(err)
+		});
+
+
+	callback(null, 'Successfully executed');
 }
