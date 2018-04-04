f19b370af766fdca4a07be00919d7aefec14d384
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-04 15:50:35 +0530

    Test SnS

--- TestingSigma/lambda.js
+++ TestingSigma/lambda.js
@@ -1,6 +1,10 @@
 let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 
+	let reciever = event['reciever'];
+	let sender = event['sender'];
+	let message = event['message']
 
+	console.log("Succesfully message ", message," for",reciever, ' from ',sender);
 	callback(null,'Successfully executed');
 }
