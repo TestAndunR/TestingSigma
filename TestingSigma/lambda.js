0856b34d66b46a131fa9794a1eb800ffa7bf52f2
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-01 15:32:35 +0530

    Add a sqs

--- TestingSigma/lambda.js
+++ TestingSigma/lambda.js
@@ -1,6 +1,19 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
+exports.handler = function (event, context, callback) {
+	let email = event.email;
 	console.log(event.email);
-	
-	callback(null,'Successfully executed');
+	sqs.sendMessage({
+		MessageBody: email,
+		QueueUrl: 'https://sqs.us-east-2.amazonaws.com/318300609668/SQS1',
+		DelaySeconds: '0',
+		MessageAttributes: {}
+	}, function (data) {
+		// your logic (logging etc) to handle successful message delivery, should be here
+	}, function (error) {
+		// your logic (logging etc) to handle failures, should be here
+	});
+
+	callback(null, 'Successfully executed');
 }
