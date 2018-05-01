0e2924744f3e638234a205f6fadd0cadd29e407a
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-01 14:58:06 +0530

    Adding an api gateway

--- TestingSigma/lambda.js
+++ TestingSigma/lambda.js
@@ -1,38 +1,6 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
-exports.handler = function (event, context, callback) {
-
-	let reciever = event['reciever'];
-	let sender = event['sender'];
-	let message = event['message']
-
-	console.log("Succesfully message ", message, " for", reciever, ' from ', sender);
-
-	sns.publish({
-		Message: message,
-		MessageAttributes: {
-			'AWS.SNS.SMS.SMSType': {
-				DataType: 'String',
-				StringValue: 'Promotional'
-			},
-			'AWS.SNS.SMS.SenderID': {
-				DataType: 'String',
-				StringValue: sender
-			},
-		},
-		PhoneNumber: reciever
-	}).promise()
-		.then(data => {
-			// your code goes here
-			console.log("Sending success to ",reciever);
-			callback(null,data);
-		})
-		.catch(err => {
-			// error handling goes here
-			console.log("sending failed",err);
-			callback(err)
-		});
-
-
-	callback(null, message);
+exports.handler = function(event, context, callback) {
+	console.log(event.email);
+	
+	callback(null,'Successfully executed');
 }
